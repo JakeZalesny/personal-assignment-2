3f00070e9737f3e474fdc51e86ee3568cf2f10a4
Jake Zalesny <jrzalesny@gmail.com>
2024-06-01 16:53:27 -0600

    added error handling

--- src/controlers/makeup.js
+++ src/controlers/makeup.js
@@ -3,60 +3,72 @@ const ObjectId = require('mongodb').ObjectId;
 // const swaggerAutogen = require('swagger-autogen')();
 
 const getAllMakeupProducts = async (req, res, next) => {
-    const result = await mongodb.getDB().db().collection('makeup').find();
-    result.toArray().then((lists) => {
+  try {
+      const result = await mongodb.getDB().db().collection('makeup').find();
+      const lists = await result.toArray();
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
-    });
-  };
-
-const getSingleMakeupProduct = async (req, res) => {
-  const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
-    const result = await mongodb
-      .getDB()
-      .db()
-      .collection('makeup')
-      .find({ _id: productId});
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    });
-  };
-
-  const createMakeupProduct = async (req, res) => {
-    const product = {
-        name: req.body.name,
-        brand: req.body.brand,
-        ingredients: req.body.ingredients,
-        score: req.body.score
-    };
-    const response = await mongodb.getDB().db().collection('makeup').insertOne(product);
-    if(response.acknowledged){
-      res.status(201).json(response);
-    }else{
-      res.status(500).json(response);
-    }
+  } catch (error) {
+      next(error); // Pass the error to the error handling middleware
+  }
 };
 
-const updateMakeupProduct = async (req, res) => {
-
-  const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
-  const product = {
-    name: req.body.name,
-    brand: req.body.brand,
-    ingredients: req.body.ingredients,
-    score: req.body.score
+const getSingleMakeupProduct = async (req, res, next) => {
+  try {
+      const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
+      const result = await mongodb.getDB().db().collection('makeup').find({ _id: productId });
+      const lists = await result.toArray();
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+  } catch (error) {
+      next(error);
   }
-  const result = await mongodb.getDB().db().collection('makeup').replaceOne({_id: productId}, product);
-  res.status(204).json(result);
-}
+};
 
-const deleteMakeupProduct = async (req, res) => {
+const createMakeupProduct = async (req, res, next) => {
+  try {
+      const product = {
+          name: req.body.name,
+          brand: req.body.brand,
+          ingredients: req.body.ingredients,
+          score: req.body.score
+      };
+      const response = await mongodb.getDB().db().collection('makeup').insertOne(product);
+      if (response.acknowledged) {
+          res.status(201).json(response);
+      } else {
+          res.status(500).json(response);
+      }
+  } catch (error) {
+      next(error);
+  }
+};
 
-  const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
-  const result = await mongodb.getDB().db().collection('makeup').deleteOne({_id: productId});
-  res.status(200).json(result);
-}
+const updateMakeupProduct = async (req, res, next) => {
+  try {
+      const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
+      const product = {
+          name: req.body.name,
+          brand: req.body.brand,
+          ingredients: req.body.ingredients,
+          score: req.body.score
+      };
+      const result = await mongodb.getDB().db().collection('makeup').replaceOne({ _id: productId }, product);
+      res.status(204).json(result);
+  } catch (error) {
+      next(error);
+  }
+};
+
+const deleteMakeupProduct = async (req, res, next) => {
+  try {
+      const productId = req.params.id.length === 24 ? new ObjectId(req.params.id) : req.params.id;
+      const result = await mongodb.getDB().db().collection('makeup').deleteOne({ _id: productId });
+      res.status(200).json(result);
+  } catch (error) {
+      next(error);
+  }
+};
 module.exports = {
   getAllMakeupProducts,
   getSingleMakeupProduct,
