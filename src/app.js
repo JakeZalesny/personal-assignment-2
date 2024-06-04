0133a848737351d1a0daedc57b31d5ce582f914c
Jake Zalesny <jrzalesny@gmail.com>
2024-06-04 10:49:59 -0600

    Added Authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./connect.js');
-const router = require('./routes')
+const router = require('./routes');
+const { auth } = require('express-openid-connect');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'gobpiUf7Lh3fZ5U8DZ6UiiI13pP5W7EU',
+  issuerBaseURL: 'https://dev-5r5raews4dzcwvlk.us.auth0.com'
+};
 
 app
   .use(bodyParser.json())
@@ -12,8 +22,18 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
+  app.use(auth(config));
+  
+  app.get('/logintest', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+  
+  // This is the route for Authentication. 
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+  
 app.use('/', router);
-
 mongodb.initDB((err, mongodb) => {
   if (err) {
     console.log(err);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 const makeupProductsController = require('../controlers/makeup');
 
